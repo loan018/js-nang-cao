2090254de0e5271113dfa1c19a5587ddf99ebd28
loan018 <loanptph52441@gmail.com>
2025-05-19 20:59:46 +0700

    Đăng nhập

--- auth.js
+++ auth.js
@@ -6,15 +6,59 @@ async function register(event) {
      alert("Hiện thị đủ thông tin");
      return;
   }
+   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    alert("Email không hợp lệ");
+    return;
+  }
+
+  if (password.length < 6) {
+    alert("Mật khẩu phải có ít nhất 6 ký tự");
+    return;
+  }
   try {
     const data ={
     email,
     password
   };
      await axios.post(`http://localhost:3000/register`,data)
-    location.href = 'index.html'
+    location.href = 'login.html'
     alert("Đăng ký thành công")
   } catch (error) {
     console.error(error)
   }
 }
+async function login(event) {
+  event.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  if(!email|| !password){
+     alert("Hiện thị đủ thông tin");
+     return;
+  }
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    alert("Email không hợp lệ");
+    return;
+  }
+
+  if (password.length < 6) {
+    alert("Mật khẩu phải có ít nhất 6 ký tự");
+    return;
+  }
+  try {
+    const data ={
+    email,
+    password
+  };
+  const res = await axios.post(`http://localhost:3000/login`, data);
+  console.log(res.data); 
+  const token = res.data.accessToken; 
+  localStorage.setItem("token", token); 
+  //location.href = 'index.html'
+  alert("Đăng nhập thành công")
+  } catch (error) {
+    console.error(error)
+  }
+}
+
